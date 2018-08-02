5a196d48ed24a6b3e0a91061a2d11e568762266b
Eduardo De La Cruz <eduardodelacruz2912@gmail.com>
2018-08-02 01:31:24 -0400

    delete class

--- src/wrestler/Application.java
+++ src/wrestler/Application.java
@@ -1,16 +1,9 @@
 package wrestler;
 
-public class App {
+public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		//testing figherView
-		
-		
 
 	}
 
